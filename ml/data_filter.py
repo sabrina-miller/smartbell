251fd1385fcc70b998b37f84c125b49c880cbb06
ssaund02 <sophie.saunders@tufts.edu>
2020-04-03 15:08:47 -0400

    Added back deleted data files, added comments to Python files

--- ml/data_filter.py
+++ ml/data_filter.py
@@ -1,3 +1,22 @@
+"""
+
+data_filter.py
+By Sophie Saunders
+Created 12/2019
+Tufts University EE-97
+Team Maximum Blue Green
+
+This script analyzes one data file and produces a count of how many 
+reps of an exercise are performed, by looking at peaks in acceleration. 
+
+Whether the exercise is a deadlift or squat is not relevant to this file. 
+
+Run this file from the command line using:
+python data_filter.py 
+
+"""
+
+#import statements
 import pandas as pd
 import csv
 import matplotlib.pyplot as plt
@@ -5,31 +24,49 @@ import statistics
 from scipy.signal import find_peaks, peak_prominences 
 from scipy.signal import detrend
 import numpy as np
+import argparse
 
-squat = pd.read_csv("data/Emily_DL_135_5reps.csv") # load in some csv data
+# establish command line argument that gives name of file to examine
+parser = argparse.ArgumentParser(description='Calculate how many reps of an exercise from a CSV file.')
+parser.add_argument("file_name")
+args = parser.parse_args()
 
-x = squat['x-axis (g)'].astype(float)
+# load in the CSV file you want to look at
+squat = pd.read_csv("data/"+args.file_name) 
+
+# convert acceleration data to float data type
+x = squat['x-axis (g)'].astype(float) 
 y = squat['y-axis (g)'].astype(float)
 z = squat['z-axis (g)'].astype(float)
 
+# merge all 3 data sets into 1 using distance formula
 s = x.pow(2) + y.pow(2) + z.pow(2)
 s = np.sqrt(s)
 
-s.plot() # plot original data
+# plot original data
+s.plot()
 plt.show()
 
+# delete datapoints where there is no change (ie, horizontal lines of no motion)
 s = s[s.diff() > 0.004]  # Try 0.003 to 0.01
 
+# perform rolling average to get neater graph
 s[:-2] = s[:-2].rolling(window=3).mean()  # Try windows 1, 2, 3
 s = s.dropna()
 
-s = s.add(-s.mean())    # Shift data to center the mean at 0
+# shift data to center the mean at zero 
+s = s.add(-s.mean())
 
-s = detrend(s) # Remove trend lines 
+# remove trend lines -- this function is built in for dataframes
+s = detrend(s)
+
+# scale data so max acceleration is at 1 
 s = s*(1/s.max())   # Scale data
 
+# find peaks in dataframe that are at least 7 data points apart
 peaks = find_peaks(s, distance=7)[0]   # Try distances: 5, 10, 15
 
+# plot the results so user can see what (if anything) went wrong 
 prominences = peak_prominences(s, peaks)[0]
 heights = s[peaks] - prominences
 plt.plot(s)
@@ -37,6 +74,8 @@ plt.plot(peaks, s[peaks], "s")
 plt.vlines(x=peaks, ymin=heights, ymax=s[peaks])
 plt.show()
 
+# count all peaks that have height >0.5* max_height
+# print this count for the user
 prominences = prominences[prominences>0.5*max(prominences)] # Try 0.4 to 0.5
 print(len(prominences))
 
--- ml/ml_smartbell.py
+++ ml/ml_smartbell.py
@@ -1,3 +1,25 @@
+"""
+
+ml_smartbell.py
+By Sophie Saunders
+For ECE Senior Design EE-98 at Tufts University
+Team Maximum Blue Green
+Adapted from tutorial at:
+https://towardsdatascience.com/human-activity-recognition-har-tutorial-with-keras-and-core-ml-part-1-8c05e365dfa0
+
+This program loops through all data files (containing x, y, z acceleration)
+and reads them into a Python dataframe, then performs neural network classification.
+
+The input to the ML algorithm is 45 acceleration values (doubles) set up as 
+[(15 x acceleration values) + (15 y accleration values) + (15 z acceleration values)]
+The output is an exercise label of either 'squat' or 'deadlift'
+
+Run this file from command line using:
+python ml_smartbell.py
+
+"""
+
+# import statements
 from __future__ import print_function
 from matplotlib import pyplot as plt
 import numpy as np
@@ -11,27 +33,21 @@ from sklearn.metrics import classification_report
 from sklearn import preprocessing
 
 import tensorflow as tf
-
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Reshape
 from keras.utils import np_utils
 
-sns.set() # Graph aesthetics
-# The number of steps within one time segment
-TIME_PERIODS = 15
-# The steps to take from one segment to the next; if this value is equal to
-# TIME_PERIODS, then there is no overlap between the segments
-STEP_DISTANCE = 8
+sns.set() # graph aesthetics
 
-# 10, 5: 94% but skewed weirdly
-# 10, 8: 93%
-# 10, 10: 92%
-# 20, 10: 93% 
-# 20, 20: 92%
-# 40, 40: 90% 
-# 40, 20: 93% Weirdly skewed also
+# Note: if STEPS_DISTANCE = TIME_PERIODS then there is no overlap between segments
+TIME_PERIODS = 15 # number of steps in one time segment
+STEP_DISTANCE = 8 # steps taken from one segment to another
 
+# read_data() function
+# takes in CSV data file path and reads it into one compiled file called all_data.csv
+# this file keeps track of the file number, who performed the exercise, the x/y/z acceleration data,
+# and what activity they performed
 def read_data(file_path):
     for index,row in enumerate(csv.reader(open('data/'+file_path, 'r'))):
         file_path = file_path.lower()
@@ -69,51 +85,54 @@ def read_data(file_path):
             prev = 0
 
     
+# machine_learn() function
+# creates .h5 machine learning model from all_data.csv 
 def machine_learn():
 
-    df = pd.read_csv('all_data.csv')
-    df['x-axis (g)'] = df['x-axis (g)'].astype(float)
+    df = pd.read_csv('all_data.csv') # create dataframe with all info from all_data.csv 
+    df['x-axis (g)'] = df['x-axis (g)'].astype(float) # convert acceleration data to float data type
     df['y-axis (g)'] = df['y-axis (g)'].astype(float)
     df['z-axis (g)'] = df['z-axis (g)'].astype(float)
-    df.drop('epoc (ms)', axis=1, inplace=True)
+    df.drop('epoc (ms)', axis=1, inplace=True) # remove timing information from dataframe
     df.drop('elapsed (s)', axis=1, inplace=True)
     
-    # Data analysis (can delete later)
-    df['activity'].value_counts().plot(kind='bar', title='Training Examples by Activity Type')
+    # Data analysis (can delete later) -- just for informational use
+    #df['activity'].value_counts().plot(kind='bar', title='Training Examples by Activity Type')
     #plt.show()
-    df['person'].value_counts().plot(kind='bar', title='Training Examples by User')
+    #df['person'].value_counts().plot(kind='bar', title='Training Examples by User')
     #plt.show()
 
-    le = preprocessing.LabelEncoder() # Convert labels from string to integer
+    le = preprocessing.LabelEncoder() # convert labels from string to integer
     df['ActivityEncoded'] = le.fit_transform(df['activity'].values.ravel()) # add encoded values to dataframe
 
-    # Differentiate between test set and training set
+    # Differentiate between files used for test set and training set
     df_test = df[df['file_num'] <= 12]
     df_train = df[df['file_num'] > 12]
     
-    # x, y, z acceleration as features
-    N_FEATURES = 3
-    # Number of steps to advance in each iteration
-    segments = []
-    labels = []
+    
+    N_FEATURES = 3 # set up x, y, z acceleration as features
+    
+    segments = [] # the input to the ML algorithm
+    labels = [] # the output to the ML algorithm
     for i in range(0, len(df_train) - TIME_PERIODS, STEP_DISTANCE):
         xs = df_train['x-axis (g)'].values[i: i + TIME_PERIODS]
         ys = df_train['y-axis (g)'].values[i: i + TIME_PERIODS]
         zs = df_train['z-axis (g)'].values[i: i + TIME_PERIODS]
         # Retrieve the most often used label in this segment
         label = stats.mode(df_train['ActivityEncoded'][i: i + TIME_PERIODS])[0][0]
-        segments.append([xs, ys, zs])
-        labels.append(label)
+        segments.append([xs, ys, zs]) # set up input as array of x, y, z accelerations
+        labels.append(label) # set up output as label of 'squat' or 'deadlift'
 
-    # Bring the segments into a better shape
+    # Reshape the inputs into arrays of the correct dimension
     x_train = np.asarray(segments, dtype= np.float32).reshape(-1, TIME_PERIODS, N_FEATURES)
     y_train = np.asarray(labels)
     
+    # print information about input, output shapes
     print('x_train shape: ', x_train.shape)
     print(x_train.shape[0], 'training samples')
     print('y_train shape: ', y_train.shape)
     
-    # Set input & output dimensions
+    # Set input and output dimensions
     num_time_periods, num_sensors = x_train.shape[1], x_train.shape[2]
     num_classes = le.classes_.size # num_classes = 2
     input_shape = (num_time_periods*num_sensors)
@@ -124,9 +143,11 @@ def machine_learn():
     x_train = x_train.astype('float32')
     y_train = y_train.astype('float32')
     
+    # use hot encoding to transform output labels into integers
     y_train_hot = np_utils.to_categorical(y_train, num_classes)
     print('New y_train shape: ', y_train_hot.shape)
     
+    # perform neural network classification
     model_m = Sequential()
     model_m.add(Reshape((TIME_PERIODS, 3), input_shape=(input_shape,)))
     model_m.add(Dense(100, activation='relu'))
@@ -136,6 +157,7 @@ def machine_learn():
     model_m.add(Dense(num_classes, activation='softmax'))
     print(model_m.summary())
     
+    # save the best algorithms as checkpoints (best means high accuracy, low val_loss)
     callbacks_list = [
     keras.callbacks.ModelCheckpoint(
         filepath='best_model.{epoch:02d}-{val_loss:.2f}.h5',
@@ -144,11 +166,11 @@ def machine_learn():
 
     model_m.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-    # Hyper-parameters
+    # hyper-parameters
     BATCH_SIZE = 200 # 200
-    EPOCHS = 50
+    EPOCHS = 15
 
-    # Enable validation to use ModelCheckpoint and EarlyStopping callbacks.
+    # perform Keras ML algorithm development
     history = model_m.fit(x_train,
                       y_train_hot,
                       batch_size=BATCH_SIZE,
@@ -157,6 +179,7 @@ def machine_learn():
                       validation_split=0.2,
                       verbose=1)
                       
+    # plot visualization of accuracy/loss over all epochs so user can see trends                  
     plt.figure(figsize=(6, 4))
     plt.plot(history.history['accuracy'], 'r', label='Accuracy of training data')
     plt.plot(history.history['val_accuracy'], 'b', label='Accuracy of validation data')
@@ -169,14 +192,16 @@ def machine_learn():
     plt.legend()
     plt.show()
 
-    # Print confusion matrix for training data
+    # print confusion matrix for training data
     y_pred_train = model_m.predict(x_train)
     # Take the class with the highest probability from the train predictions
     max_y_pred_train = np.argmax(y_pred_train, axis=1)
     print(classification_report(y_train, max_y_pred_train))
     
+    # save final model 
     model_m.save('my_model.h5')
     
+    # try using model to predict specific 
     # 0 = deadlift, 1 = squat
     # print(x_train[800]) # pick any number to try 
     #print(y_train[800])
@@ -189,15 +214,16 @@ def machine_learn():
     #coreml_prediction = coreml_model.predict({'accelData': test_record.reshape(input_shape)})
     #print(coreml_prediction["classLabel"])
     
-    
-    
-file_num = 1
-writer = csv.writer(open('all_data.csv', 'w'))
-for file in os.listdir("data/"):
+#######################################################################################################    
+
+file_num = 1 # initialize file number to start at 1
+
+writer = csv.writer(open('all_data.csv', 'w')) # create file to hold all data
+for file in os.listdir("data/"): # read in all files in the smartbell/ml/data folder
     prev = 0
     read_data(file)
     file_num +=1
 
-machine_learn()
+machine_learn() # create ML model 
 
 
